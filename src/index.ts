4834be8fb1f61e791577d5ce16c0ff84258aea24
Marcos Soares <marcossaore@gmail.com>
2025-03-24 17:42:21 -0300

    fix: imports#2

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { Dede, Register as DedeRegister, Options as DedeOptions } from './dede'
 import { Controller, Post, Put, Get, Delete, Patch, Validator, Middleware, Auth, Inject } from './decorators'
 import { UseCaseHandler } from './handlers'
 import { ServerError } from './http'
+import { Registry } from './di/registry'
 
 
 interface HttpMiddleware {
@@ -16,11 +17,26 @@ interface Validation {
     validate(input: any): any;
 }
  
-type RequestData = {
+interface RequestData {
     headers: any,
     data: any,
     middlewareData: any
 }
+
+class UseCaseHandler {
+    static load<T extends UseCase<any, any>>(
+      useCaseClass: new (...args: any[]) => T,
+      request?: RequestData
+    ): T {
+      const instance = Registry.classLoader(useCaseClass);
+      const auth = Reflect.getMetadata("auth", useCaseClass);
+      const context = request
+      if (auth && context?.middlewareData) {
+        (instance as any)[auth] = context.middlewareData[auth]
+      }
+      return instance
+    }
+  }
 
 export {
     UseCase,
