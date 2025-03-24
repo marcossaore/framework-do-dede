9939c2d53b3bcb1b1d3e2d83cd690ef1354de71b
Marcos Soares <marcossaore@gmail.com>
2025-03-24 14:51:42 -0300

    fix: export more Dede objects

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,19 @@
 import { Dede, Register as DedeRegister, Options as DedeOptions } from './dede'
 import { Controller, Post, Put, Get, Delete, Patch, Validator, Middleware, Auth, Inject } from './decorators'
+import { UseCaseHandler } from './handlers'
+import { ServerError } from './http'
+import { HttpMiddleware, Validation, UseCase } from './protocols'
+
 
 export {
     Dede,
     DedeRegister,
     DedeOptions,
+    UseCaseHandler,
+    ServerError,
+    HttpMiddleware,
+    Validation,
+    UseCase,
     Controller,
     Post,
     Put,
