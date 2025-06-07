829e3308257aa803c2c491a66a485ab0602e7877
Marcos Soares <marcossaore@gmail.com>
2025-06-07 14:27:16 -0300

    fix: improve controller with middleware

--- src/application/decorators/controller.ts
+++ src/application/decorators/controller.ts
@@ -1,36 +1,39 @@
 import 'reflect-metadata';
 import { FrameworkError } from "@/http/FrameworkError";
 import { Registry } from "@/infra/di/registry";
-import { HttpMiddleware, RequestMetricsHandler } from "@/protocols";
 
-export function Controller(basePath: string) {
+export interface MiddlewareHandler {
+    execute(input: any): Promise<any>
+}
+  
+export function Controller(basePath: string = '/') {
     return function (target: any) {
-        if (basePath === '') throw new FrameworkError('basePath cannot be empty');
+        if (!basePath) throw new FrameworkError('basePath cannot be empty');
         Reflect.defineMetadata('basePath', basePath, target);
         Registry.load(target.name, target);
     };
 }
 
-export function Middleware(middlewareClass: new (...args: any[]) => HttpMiddleware) {
+export function Middleware(middlewareClass: new (...args: any[]) => MiddlewareHandler) {
     return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
         if (!middlewareClass.prototype.execute) {
             throw new FrameworkError('Middleware must implement execute()');
         }
-        const middlewares: Array<new (...args: any[]) => HttpMiddleware> = Reflect.getMetadata('middlewares', target, propertyKey) || [];
+        const middlewares: Array<new (...args: any[]) => MiddlewareHandler> = Reflect.getMetadata('middlewares', target, propertyKey) || [];
         middlewares.push(middlewareClass);
         Reflect.defineMetadata('middlewares', middlewares, target, propertyKey);
     };
 }
 
 
-export function Middlewares(middlewareClasses: (new (...args: any[]) => HttpMiddleware)[]) {
+export function Middlewares(middlewareClasses: (new (...args: any[]) => MiddlewareHandler)[]) {
     return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
         for (const middlewareClass of middlewareClasses) {
             if (!middlewareClass.prototype.execute) {
                 throw new FrameworkError('Middleware must implement execute()');
             }
         }
-        const existingMiddlewares: Array<new (...args: any[]) => HttpMiddleware> = 
+        const existingMiddlewares: Array<new (...args: any[]) => MiddlewareHandler> = 
             Reflect.getMetadata('middlewares', target, propertyKey) || [];
         existingMiddlewares.push(...middlewareClasses);
         Reflect.defineMetadata('middlewares', existingMiddlewares, target, propertyKey);
