e9ef7a2e3489780dc468247be2ac326f9672d3d1
Marcos Soares <marcossaore@gmail.com>
2025-06-27 12:53:27 -0300

    feat: add repository protocols

--- src/index.ts
+++ src/index.ts
@@ -26,6 +26,7 @@ import { Dede, type Options, Register } from './dede'
 
 import { ServerError, NotFound, Forbidden, Conflict, Unauthorized, UnprocessableEntity, BadRequest, InternalServerError } from './http/errors/server'
 
+import type { RepositoryCreate, RepositoryUpdate, RepositoryRemove, RepositoryRemoveBy, RepositoryExistsBy, RepositoryRestoreBy, RepositoryNotExistsBy, RepositoryPagination } from './protocols/repository'
 
 export {
     Controller, Post, Get, Put, Delete, Patch, Input,
@@ -44,5 +45,7 @@ export {
 
     Dede, Options, Register,
 
-    ServerError, NotFound, Forbidden, Conflict, Unauthorized, UnprocessableEntity, BadRequest, InternalServerError
+    ServerError, NotFound, Forbidden, Conflict, Unauthorized, UnprocessableEntity, BadRequest, InternalServerError,
+
+    RepositoryCreate, RepositoryUpdate, RepositoryRemove, RepositoryRemoveBy, RepositoryExistsBy, RepositoryRestoreBy, RepositoryNotExistsBy, RepositoryPagination
 }
---
+++ src/protocols/repository.ts
@@ -0,0 +1,35 @@
+import { Entity } from "@/application";
+
+export interface RepositoryCreate<T extends Entity> {
+    create(input: T): Promise<void>
+}
+
+export interface RepositoryUpdate<T extends Entity> {
+    update(input: T): Promise<void>
+}
+
+export interface RepositoryRemove {
+    remove(id: string | number): Promise<void>
+}
+
+export type RepositoryRemoveBy<T> = {
+    [K in keyof T & string as `removeBy${Capitalize<K>}`]: (value: T[K]) => Promise<void>;
+};
+
+export type RepositoryRestoreBy<T> = {
+    [K in keyof T & string as `restoreBy${Capitalize<K>}`]: (value: T[K]) => Promise<any>;
+};
+
+export type RepositoryExistsBy<T> = {
+    [K in keyof T & string as `existsBy${Capitalize<K>}`]: (value: T[K]) => Promise<boolean>;
+};
+
+export type RepositoryNotExistsBy<T> = {
+    [K in keyof T & string as `notExistsBy${Capitalize<K>}`]: (value: T[K]) => Promise<boolean>;
+};
+
+export interface RepositoryPagination<T> {
+    restoreMany({ filter, pagination }: { filter?: Record<string, any>, pagination?: { offset: number, limit: number }}): Promise<T>
+}
+
+
