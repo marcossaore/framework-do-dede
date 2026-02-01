4fb8926f49bbb465fd1cd27e3c3eeb5cc5411a1d
Marcos Soares <marcossaore@gmail.com>
2026-02-01 00:02:06 -0300

    fix: storage as lazy load#2

--- src/application/services.ts
+++ src/application/services.ts
@@ -7,13 +7,14 @@ export interface StorageGateway {
   delete(key: string): Promise<boolean>
 }
 
-export function Storage(gatewayName: string, container: Container = DefaultContainer) {
+export function Storage(gatewayName: string, container?: Container) {
   return function (target: any, propertyKey: string): void {
     Object.defineProperty(target, propertyKey, {
       get: function () {
         return new Proxy({}, {
           get(_: any, prop: string) {
-            const dependency = container.inject(gatewayName);
+            const resolvedContainer = container ?? DefaultContainer;
+            const dependency = resolvedContainer.inject(gatewayName);
             if (!dependency?.save || !dependency?.get || !dependency?.delete) {
               throw new Error(`${gatewayName} is not a valid StorageGateway`);
             }
